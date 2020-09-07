7489ccdf64855a7383476e7a539595f5f0bbbf12
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-07 16:09:46 +0200

    Adds contact page with template driven form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from './landing/landing.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ContactComponent} from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [	
+  {path:'', component:LandingComponent},
+  {path:'contact', component:ContactComponent},
+  {path: '**', component:PageNotFoundComponent}
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { GoPreviousPageDirective } from './go-previous-page.directive';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     PageNotFoundComponent,
-    GoPreviousPageDirective
+    GoPreviousPageDirective,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
