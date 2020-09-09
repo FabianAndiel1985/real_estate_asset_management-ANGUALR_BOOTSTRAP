f8fc9f6a36dc541793f3c58807f27e9fc243d7f3
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-09 12:44:28 +0200

    adds getting query params thankyou-message component

--- src/app/thankyou/thank-you-message/thank-you-message.component.ts
+++ src/app/thankyou/thank-you-message/thank-you-message.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-thank-you-message',
   templateUrl: './thank-you-message.component.html',
   styleUrls: ['./thank-you-message.component.css']
 })
+
 export class ThankYouMessageComponent implements OnInit {
+	
+	firstname:string;
+	lastname:string;	
+  	action:string;
 
-  constructor() { }
+  	constructor(private currentRoute:ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+	  ngOnInit() {
+	  	if (this.currentRoute.snapshot.queryParams['action'] == "contact" ) 
+	  	{ 	
+	  		this.firstname = this.currentRoute.snapshot.queryParams['firstname']; 
+	  		this.lastname = this.currentRoute.snapshot.queryParams['lastname'];
+	  		this.action = this.currentRoute.snapshot.queryParams['action']+"ing";
+	 	}
+	  }
 
 }
