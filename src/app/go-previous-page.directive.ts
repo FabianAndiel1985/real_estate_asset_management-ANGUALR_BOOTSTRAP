efb4cc8f44f36d319a06d935ad8fd3fd4d74f5b9
Fabian Andiel <fabian.andiel@yahoo.com>
2020-10-01 14:17:53 +0200

    adds card titles to the the firebase data and displays them on the estate detail

--- src/app/go-previous-page.directive.ts
+++ src/app/go-previous-page.directive.ts
@@ -10,7 +10,7 @@ export class GoPreviousPageDirective {
 
   constructor(private location:Location) { }
 
-  @HostListener('click',)
+  @HostListener('click')
   handleKeyDown(event: KeyboardEvent) {
     this.location.back();
   }
