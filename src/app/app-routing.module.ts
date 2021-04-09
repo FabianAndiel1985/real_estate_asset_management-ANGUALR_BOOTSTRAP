00145dc2c88433e2f88534282fdc78770b36edaf
Fabian Andiel <fabian.andiel@yahoo.com>
2021-04-09 19:56:11 +0200

    adds route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import {LandingComponent} from './landing/landing.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {ContactComponent} from './contact/contact.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:'', component:LandingComponent},
-  {path:'contact', component:ContactComponent},
+  {path:'contact',
+  canActivate:[AuthGuard],
+  component:ContactComponent},
   {
   	path:'real-estate',
-  	loadChildren:()=>import("./real-estate/real-estate.module").then(m=>m.RealEstateModule)
+  	loadChildren:()=>import("./real-estate/real-estate.module").then(m=>m.RealEstateModule),
   },
   {path:'admin-login', component:AuthComponent},
   {path: '**', component:PageNotFoundComponent}
